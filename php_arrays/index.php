ebbe34529cc80ff72c387f9d5a263baf14ec4a2c
Bahadir Biriktir <bahadirbrktr@gmail.com>
2023-03-09 22:57:58 +0100

    PHP

---
+++ php_arrays/index.php
@@ -0,0 +1,20 @@
+
+<?php
+$Basketball = array("Curry", "Lebron", "Harden", "Durant", "Irving", "Simons");
+$Password_numbers = [12, 456, 34, 85, 74,48];
+$GPU = array("2080Super", "3060ti", 2080, 1060, 1080, 2070);
+$Password_numbers [2] = 1156;
+
+echo "Variable: Basketball" ."<br>";
+echo $Basketball [1] ."<br>";
+echo $Basketball [4]."<br>"."<br>";
+
+echo "Variable: Password_numbers" ."<br>";
+echo $Password_numbers [0]. "<br>";
+echo $Password_numbers [2] . "<br>" . "<br>";
+
+echo "Variable: GPU" ."<br>";
+echo $GPU [1]."<br>";
+echo $GPU [2]."<br>";
+echo $GPU [5]."<br>";
+?>
